afaf9d6f366cdfaf1aa0fb012cda1e16ee243033
esviren <st-rodriguez@hotmail.com>
2024-06-26 16:02:19 -0500

    create cake page & component with .vue

---
+++ .nuxt/types/schema.d.ts
@@ -0,0 +1,124 @@
+import { NuxtModule, RuntimeConfig } from 'nuxt/schema'
+declare module 'nuxt/schema' {
+  interface NuxtConfig {
+    ["icon"]?: typeof import("nuxt-icon").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["colorMode"]?: typeof import("@nuxtjs/color-mode").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["tailwindcss"]?: typeof import("@nuxtjs/tailwindcss").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["ui"]?: typeof import("@nuxt/ui").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["i18n"]?: typeof import("@nuxtjs/i18n").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["pinia"]?: typeof import("@pinia/nuxt").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["primevue"]?: typeof import("nuxt-primevue").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["devtools"]?: typeof import("@nuxt/devtools").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    modules?: (undefined | null | false | NuxtModule | string | [NuxtModule | string, Record<string, any>] | ["nuxt-icon", Exclude<NuxtConfig["icon"], boolean>] | ["@nuxtjs/color-mode", Exclude<NuxtConfig["colorMode"], boolean>] | ["@nuxtjs/tailwindcss", Exclude<NuxtConfig["tailwindcss"], boolean>] | ["@nuxt/ui", Exclude<NuxtConfig["ui"], boolean>] | ["@nuxtjs/i18n", Exclude<NuxtConfig["i18n"], boolean>] | ["@pinia/nuxt", Exclude<NuxtConfig["pinia"], boolean>] | ["nuxt-primevue", Exclude<NuxtConfig["primevue"], boolean>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
+  }
+  interface RuntimeConfig {
+   app: {
+      baseURL: string,
+
+      buildAssetsDir: string,
+
+      cdnURL: string,
+   },
+  }
+  interface PublicRuntimeConfig {
+   i18n: {
+      baseUrl: string,
+
+      defaultLocale: string,
+
+      defaultDirection: string,
+
+      strategy: string,
+
+      lazy: boolean,
+
+      rootRedirect: any,
+
+      routesNameSeparator: string,
+
+      defaultLocaleRouteNameSuffix: string,
+
+      skipSettingLocaleOnNavigate: boolean,
+
+      differentDomains: boolean,
+
+      trailingSlash: boolean,
+
+      configLocales: Array<any>,
+
+      locales: any,
+
+      detectBrowserLanguage: {
+         alwaysRedirect: boolean,
+
+         cookieCrossOrigin: boolean,
+
+         cookieDomain: any,
+
+         cookieKey: string,
+
+         cookieSecure: boolean,
+
+         fallbackLocale: string,
+
+         redirectOn: string,
+
+         useCookie: boolean,
+      },
+
+      experimental: {
+         localeDetector: string,
+
+         switchLocalePathLinkSSR: boolean,
+
+         autoImportTranslationFunctions: boolean,
+      },
+   },
+
+   primevue: {
+      usePrimeVue: boolean,
+
+      resolvePath: any,
+
+      cssLayerOrder: string,
+
+      importPT: any,
+
+      options: any,
+
+      components: Array<{
+
+      }>,
+
+      directives: Array<{
+
+      }>,
+
+      composables: Array<{
+
+      }>,
+
+      config: Array<{
+
+      }>,
+
+      services: Array<{
+
+      }>,
+
+      styles: Array<{
+
+      }>,
+
+      injectStylesAsString: Array<any>,
+
+      injectStylesAsStringToTop: Array<string>,
+   },
+  }
+}
+declare module 'vue' {
+        interface ComponentCustomProperties {
+          $config: RuntimeConfig
+        }
+      }
